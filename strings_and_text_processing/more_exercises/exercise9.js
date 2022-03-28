6151457389fc847d5d3cc6bd62fa1f3ffa380b6b
Rodshell Fleurinord <rodshellfleurinord@gmail.com>
2022-03-28 10:51:34 -0700

    inital commit

--- strings_and_text_processing/more_exercises/exercise9.js
+++ strings_and_text_processing/more_exercises/exercise9.js
@@ -1,23 +1,10 @@
-function wordLengths(string) {
-  if (string) {
-    return string.split(' ').map(word => {
-      return word + ' ' + word.length
-    });
-  }
-  return [];
+function searchWord(word, text) {
+  const regex = new RegExp(word, 'gi');
+  const matches = text.match(regex);
+
+  return matches ? matches.length : 0;
 }
 
-console.log(wordLengths('cow sheep chicken'));
-// ["cow 3", "sheep 5", "chicken 7"]
+const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?';
 
-console.log(wordLengths('baseball hot dogs and apple pie'));
-// ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
-
-console.log(wordLengths("It ain't easy, is it?"));
-// ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
-
-console.log(wordLengths('Supercalifragilisticexpialidocious'));
-// ["Supercalifragilisticexpialidocious 34"]
-
-console.log(wordLengths(''));      // []
-console.log(wordLengths());        // []
+console.log(searchWord('sed', text));      // 3
